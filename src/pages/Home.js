73eaed9cb4e374b53502376292ba9ad52e36aa09
Ryan Nurwono <ryan_nurwono@berkeley.edu>
2022-06-15 17:41:53 -0700

    test1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import Todos from "../component/todos";
 import AddTodo from "../component/AddTodo";
 import "../pages/Home.css";
-
+import { arrayIncludes } from "@mui/x-date-pickers/internals/utils/utils";
+function Header(props){
+  return <h1>Todo's Left for {props.name}</h1>
+}
 class Home extends Component {
   // Create a default state of this component with an empty list of todos.
   constructor() {
@@ -11,6 +14,8 @@ class Home extends Component {
       todos: [],
     };
   }
+  
+  
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -21,22 +26,38 @@ class Home extends Component {
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
+    const todoContent = this.state.todos.map(item => item.content);
+    if (!(todoContent.find(content => todo.content === content))){
+      let new_list = [...this.state.todos, todo];
+      this.setState({
+        todos: new_list,
+      });
+    }
+    
+    
     // Update the local state with the new array.
-    this.setState({
-      todos: new_list,
-    });
+    
   };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+};
+
   render() {
     return (
       <div className="Home">
-        <h1>Todo's </h1>
+        <Header name = "Ryan" ></Header>
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo = {this.deleteTodo} />
       </div>
     );
   }
